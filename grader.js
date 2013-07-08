16a36013d249b65acd45e37cdb53362c59eb4521
Bendik Solheim <bendiksolheim@gmail.com>
2013-07-08 21:55:17 +0200

    Added restler. Added functionality for checking URLs as well as files

--- grader.js
+++ grader.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = 'index.html';
@@ -16,6 +17,13 @@ var assertFileExists = function(file) {
 	return instr;
 };
 
+var assertUrl = function(url) {
+	var isUrl = url.indexOf("http") === 0;
+	if (!isUrl) process.exit(1);
+
+	return url;
+};
+
 var cheerioHtmlFile = function(htmlFile) {
 	return cheerio.load(fs.readFileSync(htmlFile));
 };
@@ -24,31 +32,43 @@ var loadChecks = function(checksFile) {
 	return JSON.parse(fs.readFileSync(checksFile));
 };
 
-var checkHtmlFile = function(htmlFile, checksFile) {
-	$ = cheerioHtmlFile(htmlFile);
+var checkHtml = function($, checksFile) {
 	var checks = loadChecks(checksFile).sort();
 	var out = {};
 	for (var check in checks) {
 		var present = $(checks[check]).length > 0;
 		out[checks[check]] = present;
 	}
+	console.log(JSON.stringify(out, null, 4));
+};
 
-	return out;
+var checkHtmlFile = function(htmlFile, checksFile) {
+	$ = cheerioHtmlFile(htmlFile);
+	checkHtml($, checksFile);
+};
+
+var checkUrl = function(url, checksFile) {
+	rest.get(url).on('complete', function(result) {
+		$ = cheerio.load(result);
+		checkHtml($, checksFile);
+	});
 };
 
 var clone = function(fn) {
 	// Workaround for commander.js issue
 	// http://stackoverflow.com/a/6772648
-	fn.bind({});
+	return fn.bind({});
 };
 
 if (require.main == module) {
 	program.option('-c --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 		.option('-f --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+		.option('-u --url <url>', "URL to check", clone(assertUrl))
 		.parse(process.argv);
-	var checkJson = checkHtmlFile(program.file, program.checks);
-	var outJson = JSON.stringify(checkJson, null, 4);
-	console.log(outJson);
+	if (typeof program.url !== 'undefined')
+		checkUrl(program.url, program.checks);
+	else
+		checkHtmlFile(program.file, program.checks);
 } else {
 	exports.checkHtmlFile = checkHtmlFile;
 }
