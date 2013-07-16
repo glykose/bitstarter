a2f0a702ef2d82de465b1606c3d162bbdf5f1fba
Bendik Solheim <bendiksolheim@gmail.com>
2013-07-16 21:34:50 +0200

    Restyled index. Added static folders to express to serve css and js. Added assets

--- web.js
+++ web.js
@@ -3,12 +3,16 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/font', express.static(__dirname + '/font'));
+app.use('/js', express.static(__dirname + '/js'));
+
 app.get('/', function(request, response) {
 	var contents = fs.readFileSync('index.html');
 	response.send(contents.toString("utf-8"));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
 	console.log("Listening on " + port);
 });
